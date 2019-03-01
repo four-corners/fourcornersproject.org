6ba0204e74ae9dbcccbfc5969a116a1d12139b16
coreytegeler <coreytegeler@gmail.com>
2019-03-01 15:09:38 -0500

    add fathom analytics

--- index.php
+++ index.php
@@ -23,4 +23,19 @@
 		</div>			
 	</body>
 	<?php wp_footer(); ?>
+	<!-- Fathom - simple website analytics - https://github.com/usefathom/fathom -->
+		<script>
+		(function(f, a, t, h, o, m){
+			a[h]=a[h]||function(){
+				(a[h].q=a[h].q||[]).push(arguments)
+			};
+			o=f.createElement('script'),
+			m=f.getElementsByTagName('script')[0];
+			o.async=1; o.src=t; o.id='fathom-script';
+			m.parentNode.insertBefore(o,m)
+		})(document, window, '//162.243.9.116/tracker.js', 'fathom');
+		fathom('set', 'siteId', 'ECXIG');
+		fathom('trackPageview');
+		</script>
+	<!-- / Fathom -->
 </html>
